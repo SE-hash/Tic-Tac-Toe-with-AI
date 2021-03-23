fa0687acb1448ba74f0535d59b725393c9362473
frank <63493326+SE-hash@users.noreply.github.com>
2021-03-23 18:58:48 +0800

    easy level

--- src/Tic_Tac_Toe_with_AI.java
+++ src/Tic_Tac_Toe_with_AI.java
@@ -1,95 +1,102 @@
+import java.util.Random;
 import java.util.Scanner;
 
-public class Tic_Tac_Toe_with_AI {
+
+class Tic_Tac_Toe_with_AI {
     public static void main(String[] args) {
+        String cells = "         ";
+
+        paint(cells, -1, -1);//打印空棋盘
+
+        play(cells);
+    }
+
+    /**
+     * 游戏主菜单
+     *
+     * @param cells 棋盘
+     */
+    static void play(String cells) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter the cells: ");
-        String cells = scan.nextLine().replace('_', ' ');
+        Random random = new Random();
 
-        System.out.println("---------");
-        for (int i = 0; i < 9; i++) {
-            if (i % 3 == 0) {
-                System.out.print("| ");
-            }
-            if (cells.charAt(i) == '_') {
-                System.out.print("  ");
-            } else {
-                System.out.print(cells.charAt(i) + " ");
-            }
-            if (i % 3 == 2) {
-                System.out.println("|");
-            }
-        }
-        System.out.println("---------");
-
-        int turns;
-        if (judge(cells) == 'X') {
-            turns = 1;
-        } else turns = 2;
+        String coordinate;
+        int coordinateX;
+        int coordinateY;
+        int marks;
+        int turns = 1;//奇X偶O
 
 
         while (true) {
-            int coordinateX;
-            int coordinateY;
-            int marks;
-            System.out.print("Enter the coordinates: ");
-            if (scan.hasNextInt()) {
-                String coordinate = scan.nextLine();
-                coordinateX = (coordinate.charAt(0) - '0');
-                coordinateY = (coordinate.charAt(2) - '0');
-                marks = (coordinateX - 1) * 3 + (coordinateY - 1);
-                if (coordinateX > 3 || coordinateX < 0 || coordinateY > 3 || coordinateY < 0) {
-                    System.out.println("Coordinates should be from 1 to 3!");
-                    continue;
-                }
-                if (cells.charAt(marks) != ' ') {
-                    System.out.println("This cell is occupied! Choose another one!");
-                    continue;
-                }
+            if ((turns & 1) == 1) {
+                System.out.print("Enter the coordinates: ");
+                if (scan.hasNextInt()) {
+                    coordinate = scan.nextLine();
+                    coordinateX = (coordinate.charAt(0) - '0');
+                    coordinateY = (coordinate.charAt(2) - '0');
+                    marks = (coordinateX - 1) * 3 + (coordinateY - 1);
 
-                cells = paintX(cells, marks, turns);
+                    if (coordinateX > 3 || coordinateX < 0 || coordinateY > 3 || coordinateY < 0) {
+                        System.out.println("Coordinates should be form 1 to 3!");
+                        continue;
+                    }
+                    if (cells.charAt(marks) != ' ') {
+                        System.out.println("This cell is occupied! Choose another one!");
+                        continue;
+                    }
 
-                boolean gameNotFinished;
-                boolean draw;
-                boolean impossible;
-                boolean xWin;
-                boolean oWin;
+                    cells = paint(cells, marks, turns);
+                    turns++;
 
-                xWin = check(cells, 'X');
-                oWin = check(cells, 'O');
-                impossible = (xWin && oWin) || count(cells);
-                draw = (isMax(cells)) && !xWin && !oWin;
-                gameNotFinished = !xWin && !oWin && !draw;
-
-                if (impossible) {
-                    System.out.println("Impossible");
-                    return;
-                }
-
-                if (xWin) {
-                    System.out.println("X wins");
-                    return;
-                }
-                if (oWin) {
-                    System.out.println("O wins");
-                    return;
-                }
-                if (draw) {
-                    System.out.println("Draw");
-                    return;
-                }
-                if (gameNotFinished) {
-                    System.out.println("Game not finished");
-                    return;
+                } else {
+                    System.out.println("You should enter numbers!");
+                    scan.nextLine();
                 }
             } else {
-                System.out.println("You should enter numbers!");
-                scan.nextLine();
+                marks = random.nextInt(9) ;
+                System.out.println("Making move level \"easy\"");
+                while (cells.charAt(marks) != ' ') {
+                    marks = random.nextInt(9);
+                }
+                cells = paint(cells, marks, turns);
+                turns++;
+            }
+            boolean draw;
+            boolean impossible;
+            boolean xWin;
+            boolean oWin;
+
+            xWin = check(cells, 'X');
+            oWin = check(cells, 'O');
+            impossible = (xWin && oWin) || count(cells);
+            draw = (isMax(cells)) && !xWin && !oWin;
+
+            if (impossible) {
+                System.out.println("Impossible");
+                return;
+            }
+
+            if (xWin) {
+                System.out.println("X wins");
+                return;
+            }
+            if (oWin) {
+                System.out.println("O wins");
+                return;
+            }
+            if (draw) {
+                System.out.println("Draw");
+                return;
             }
         }
     }
 
-    static String paintX(String cells, int marks, int turns) {
+    /**
+     * @param cells 棋盘
+     * @param marks 下棋点
+     * @param turns 计算此轮谁下棋
+     */
+    static String paint(String cells, int marks, int turns) {
         System.out.println("---------");
         for (int i = 0; i < 9; i++) {
             if (i % 3 == 0) {
@@ -109,6 +116,7 @@ public class Tic_Tac_Toe_with_AI {
             } else {
                 System.out.print(cells.charAt(i) + " ");
             }
+
             if (i % 3 == 2) {
                 System.out.println("|");
             }
@@ -117,6 +125,11 @@ public class Tic_Tac_Toe_with_AI {
         return cells;
     }
 
+    /**
+     * @param cells 棋盘
+     * @param a     取X或者O
+     * @return 检查字符a是否胜利
+     */
     static boolean check(String cells, char a) {
         if (cells.charAt(0) == a && cells.charAt(0) == cells.charAt(1) && cells.charAt(1) == cells.charAt(2)) {
             return true;
@@ -135,6 +148,10 @@ public class Tic_Tac_Toe_with_AI {
         } else return cells.charAt(2) == a && cells.charAt(2) == cells.charAt(4) && cells.charAt(4) == cells.charAt(6);
     }
 
+    /**
+     * @param cells 棋盘
+     * @return 检查棋盘中X和O的数量，若其中一个比另外一数量多出1以上返回true
+     */
     static boolean count(String cells) {
         int x = 0;
         int o = 0;
@@ -149,24 +166,10 @@ public class Tic_Tac_Toe_with_AI {
         return Math.abs(x - o) > 1;
     }
 
-    static char judge(String cells) {
-        int x = 0;
-        int o = 0;
-        for (int i = 0; i < cells.length(); i++) {
-            if (cells.charAt(i) == 'X') {
-                x++;
-            }
-            if (cells.charAt(i) == 'O') {
-                o++;
-            }
-        }
-        if (x <= o) {
-            return 'X';
-        } else {
-            return 'O';
-        }
-    }
-
+    /**
+     * @param cells 棋盘
+     * @return 如果棋盘中没有可以下子的地方，返回true
+     */
     static boolean isMax(String cells) {
         int num = 0;
         for (int i = 0; i < cells.length(); i++) {
@@ -174,6 +177,6 @@ public class Tic_Tac_Toe_with_AI {
                 num++;
             }
         }
-        return num == 0;
+        return num <= 0;
     }
 }
